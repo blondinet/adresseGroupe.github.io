42d01e4a542d2742b93fb358974ea4dc4166297e
Gahel <music@gahel.fr>
2024-06-23 04:07:14 +0200

    wip: Transfer de la base google spreadsheet vers firebase
    erreurs pour certains index, valeurs melangees

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { db } from './firebase-config.js';
-import { collection, addDoc, getDocs, query, where, updateDoc, deleteDoc, Timestamp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { collection, addDoc, getDocs, query, where, updateDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 // Read data
 async function readData() {
@@ -8,7 +8,7 @@ async function readData() {
         querySnapshot.forEach((doc) => {
             const data = doc.data();
             console.log("Document ID:", doc.id, "Index:", data.index, "Cluster:", data.cluster, 
-                "Date de passage:", data.date_de_passage.toDate(), 
+                "Date de passage:", data.date_de_passage,
                 "Assigné:", data.assigned);
             return doc;
         });
@@ -28,7 +28,7 @@ async function writeData(index, cluster, date_de_passage, assigned) {
             const docRef = await addDoc(collection(db, "adresseTable"), {
                 index: index,
                 cluster: cluster,
-                date_de_passage: Timestamp.fromDate(new Date(date_de_passage)),
+                date_de_passage: date_de_passage,
                 assigned: assigned
             });
             console.log("Document written with ID: ", docRef.id);
@@ -44,10 +44,6 @@ async function writeData(index, cluster, date_de_passage, assigned) {
 // Update data
 async function updateData(index, newData) {
     try {
-        if (newData.date_de_passage) {
-            newData.date_de_passage = Timestamp.fromDate(new Date(newData.date_de_passage));
-        }
-
         const q = query(collection(db, "adresseTable"), where("index", "==", index));
         const querySnapshot = await getDocs(q);
 
@@ -91,7 +87,7 @@ async function findDataByIndex(index) {
             const doc = querySnapshot.docs[0];
             const data = doc.data();
             console.log("Found document - Index:", data.index, "Cluster:", data.cluster, 
-                "Date de passage:", data.date_de_passage.toDate(), 
+                "Date de passage:", data.date_de_passage,
                 "Assigné:", data.assigned);
         } else {
             console.log("No document found with index: " + index);
@@ -99,6 +95,56 @@ async function findDataByIndex(index) {
     } catch (error) {
         console.error("Error finding document: ", error);
     }
+}
+
+
+async function transferVisits(spreadsheetURL) {
+    fetch(spreadsheetURL)
+        .then(response => response.text())
+        .then(data => {
+            // Parse the CSV data
+            const rows = data.split('\n');
+            const csvArray = rows.map(row => row.split(','));
+            let fetchedDataVisited = csvArray;
+
+            // check if group has been assigned
+            let isAssigned = Array(fetchedDataVisited.length).fill(false);
+
+            // check if group has a date
+            let dates = Array(fetchedDataVisited.length).fill('');
+
+            // iterate over data except first row which contains csv headers
+            // compute the dates and assignments for each row as some are omitted
+            fetchedDataVisited.slice(1).forEach((visit, i) => {
+                let index = visit[0];
+                let group = visit[2];
+                let date = visit[3];
+                let assigned = visit[4];
+
+                if (
+                    date !== '' &&
+                    date !== '\r'
+                ) {
+                    dates[index] = date;
+                }
+
+                if (assigned === 'TRUE') {
+                    isAssigned[index] = date;
+                }
+            });
+
+            // write to database
+            fetchedDataVisited.slice(1).forEach((visit, i) => {
+                let index = visit[0];
+                let group = visit[2];
+                let date = dates[index];
+                let assigned = isAssigned[index];
+                writeData(index, group, date, assigned);
+            });
+        })
+        .catch(error => {
+            console.error('Erreur lors de la récupération des données :', error);
+        });
 }
 
 
@@ -111,4 +157,4 @@ window.findDataByIndex = findDataByIndex;
 
 
 // Export functions to make them accessible
-export { readData, writeData, updateData, deleteData, findDataByIndex };
+export { readData, writeData, updateData, deleteData, findDataByIndex, transferVisits};
